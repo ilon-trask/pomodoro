2d64e9980c10b9a1fd281bd765547b034eb495d4
ilon-trask <misha007zyt@gmail.com>
2025-01-13 20:15:43 +0100

    added notion to the project

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
 import Areas from "@/components/Areas";
 import Timer from "@/components/Timer";
+import { notion } from "@/lib/notion";
 
 export default function Home() {
+  (async () => {
+    const listUsersResponse = await notion.users.list({});
+    console.log(listUsersResponse);
+  })();
   return (
     <div className="min-h-screen flex justify-center items-center">
       <div className="flex gap-4">
